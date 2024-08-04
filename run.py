420a2e040a8373f9ed9b56612e189e6818238fda
VyanPersad <vyanpersad@gmail.com>
2024-08-03 20:59:20 -0400

    Some more testing.

--- run.py
+++ run.py
@@ -5,4 +5,9 @@ url ='https://en.wikipedia.org/wiki/2024_Summer_Olympics_medal_table'
 
 df = simple_scraper(url=url, test=1, table_Num=3)
 
-#write_to_csv(df, 'test')
+write_to_csv(df, destpath='dataSets', file_name='test')
+
+df2 = read_from_file('dataSets\sales1.xlsx')
+print(df2)
+#write_to_csv(df2, destpath='dataSets', file_name='test')
+df2.to_csv('dataSets\test.csv')
